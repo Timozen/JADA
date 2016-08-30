15e22f299bc875d8db27b93e6cf56b91d423d5df
tim.buechner <tim.buechner@uni-jena.de>
2016-08-30 19:59:33 +0200

    Main Test

--- src/com/jada/Main.java
+++ src/com/jada/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Synched?");
         System.out.println("Another test");
+        System.out.println("and again");
     }
 }
